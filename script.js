7fab28bc569d9f10faebb5711a66cc9a889bf564
Vinicius Esmeraldo <vinicius.esmeraldodev@gmail.com>
2025-03-31 16:45:35 -0300

    Feito alguns ajustes

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 let totalSlides = document.querySelectorAll('.slider--item').length;
 let currentSlide = 0;
 
-let sliderWidth = document.querySelector('.slider').clientWidth;
-
-document.querySelector('.slider--width').style.width = 
-    `${sliderWidth * totalSlides}px`;
-
-document.querySelector('.slider--controls').style.width = 
-    `${sliderWidth}px`;
-document.querySelector('.slider--controls').style.height = 
+document.querySelector('.slider--width').style.width =
+    `calc(100vw * ${totalSlides})`;
+document.querySelector('.slider--controls').style.height =
     `${document.querySelector('.slider').clientHeight}px`;
 
 function goPrev() {
@@ -33,4 +28,4 @@ function updateMargin() {
         `-${newMargin}px`;
 }
 
-setInterval(goNext, 5000);
+setInterval(goNext, 4000);
